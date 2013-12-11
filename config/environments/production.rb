bbc798bf4489fb02e658d66ef6edbab02a38280d
Anujan Panchadcharam <Anujan714@gmail.com>
2013-12-11 17:58:59 -0500

    Subdomaining for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ Shopkick::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_dispatch.tld_length = 2
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
