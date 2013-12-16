6d5d438d130e94e05f68d83b9bf72c642fdb5f72
Anujan Panchadcharam <Anujan714@gmail.com>
2013-12-15 19:48:08 -0500

    Fixed an issue with Promotion validation

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -8,13 +8,22 @@ class Promotion < ActiveRecord::Base
   validates :amount, :applies_to, :code, :start_date, :option, presence: true
   validates :option, inclusion: { in: OPTIONS }
   validates :applies_to, inclusion: { in: APPLIES_TO }, unless: :free_shipping?
-  validates :applies_to, inclusion: { in: shipping_options }, if: :free_shipping?
   validates :amount, numericality: true
   validates :applies_to_arg, presence: true, if: :has_applies_to_arg?
   validates :code, uniqueness: true
 
   has_many :order_promotions
   has_many :orders, through: :order_promotions, source: :order
+
+  def self.shipping_options
+  	defaults = ["All countries"]
+  	if Profile.first
+  		defaults += [Profile.first.address_country, "Rest of the world"]
+  	end
+
+  	defaults
+  end
+  validates :applies_to, inclusion: { in: Promotion.shipping_options }, if: :free_shipping?
 
   def applies_to_arg?
   	self.applies_to != 'all orders'
@@ -41,37 +50,25 @@ class Promotion < ActiveRecord::Base
   end
 
   def to_s
-  	when self.option
-  		case 'shipping'
-  			"Free shipping for any shipping rate that is less than or equal to #{amount} and applies to #{applies_to}"
-			case 'dollar'
-				applies_to_str("Take $#{amount} off for #{applies_to} ")
-			case 'percent'
-				applies_to_str("Take #{amount}% off for #{applies_to} ")
-			end
-  	end
+  	case self.option
+		when 'shipping'
+			"Free shipping for any shipping rate that is less than or equal to #{amount} and applies to #{applies_to}"
+		when 'dollar'
+			applies_to_str("Take $#{amount} off for #{applies_to} ")
+		when 'percent'
+			applies_to_str("Take #{amount}% off for #{applies_to} ")
+		end
   end
 
   def applies_to_str(str='')
-  	when applies_to
-			case 'orders_over'
-				str += "$#{applies_to_arg}"
-			case 'specific product'
-				str += Product.find(applies_to_arg).title
-			case 'specific category'
-				str += Category.find(applies_to_arg).title
-			end
+  	case applies_to
+		when 'orders_over'
+			str += "$#{applies_to_arg}"
+		when 'specific product'
+			str += Product.find(applies_to_arg).title
+		when 'specific category'
+			str += Category.find(applies_to_arg).title
 		end
-
 		str
-  end
-
-  def shipping_options
-  	defaults = ["All countries"]
-  	if Profile.first
-  		defaults += [Profile.first.address_country, "Rest of the world"]
-  	end
-
-  	defaults
   end
 end
