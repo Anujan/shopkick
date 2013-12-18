0e4e06cacb21e3ef985edbdc5704e030eb040f5e
Anujan Panchadcharam <Anujan714@gmail.com>
2013-12-18 16:37:32 -0500

    Chart

--- app/assets/javascripts/admin/routers/dashboard_router.js
+++ app/assets/javascripts/admin/routers/dashboard_router.js
@@ -5,22 +5,6 @@ Shopkick.Routers.Dashboard = Backbone.Router.extend({
   },
 
   dashboard: function() {
-    var data = [['Date', 'Total Price']];
-    var values = [];
-    var self = this;
-    Shopkick.dataSeries.fetch({
-      success: function() {
-        values = Shopkick.dataSeries.map(function(model) {
-          return model.point();
-        });
-        data = data.concat(values);
-        self._swapView(new Backbone.GoogleChart({
-          chartType: 'LineChart',
-          dataTable: data,
-          options: {'title': 'ORDERS BY DAY', 'pointSize': 5}
-        }));
-      },
-      wait: true
-    });
+    this._swapView(new Shopkick.Views.Dashboard);
   }
 });
---
+++ app/assets/javascripts/admin/views/dashboard/dashboard.js
@@ -0,0 +1,29 @@
+Shopkick.Views.Dashboard = Backbone.View.extend({
+  initialize: function() {
+    this.listenTo(Shopkick.dataSeries, 'sync', this.order_day_chart);
+  },
+
+  template: JST['dashboard/index'],
+
+  render: function() {
+    this.$el.html(this.template());
+    Shopkick.dataSeries.fetch();
+    return this;
+  },
+
+  order_day_chart: function() {
+    var data = [['Date', 'Total Price']];
+    var values = [];
+    var self = this;
+    values = Shopkick.dataSeries.map(function(model) {
+      return model.point();
+    });
+    data = data.concat(values);
+    var view = new Backbone.GoogleChart({
+      chartType: 'LineChart',
+      dataTable: data,
+      options: {'title': 'ORDERS BY DAY', 'pointSize': 5}
+    });
+    self.$("#order-day-chart").html(view.render().$el);
+  }
+});
